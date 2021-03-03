544fb0a3812b34af7a583036d20e133fe3476a27
An <studentsancho@gmail.com>
2021-03-03 23:55:50 +0900

    Done coding the bone -> calibrate params

--- Pytorch/Projects/FGSM/FGSM.py
+++ Pytorch/Projects/FGSM/FGSM.py
@@ -13,11 +13,11 @@ import torchvision.transforms as transforms
 import torch.nn.functional as f
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 # Hyper-parameters
-learning_rate = 1e-5
-batch_size = 128
-
+learning_rate = 1e-4
+batch_size = 64
 if torch.cuda.is_available():
     device = 'cuda'
 else:
@@ -39,6 +39,7 @@ class ConvNet(nn.Module):
     def __init__(self):
         super(ConvNet, self).__init__()
 
+        '''
         # 1st conv layer = 32 3*3 filters, with 2*2 stride
         # CNN => ReLU => Batch Normalization(which is skipped in this code)
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(3, 3), stride=(2, 2))
@@ -57,8 +58,19 @@ class ConvNet(nn.Module):
         # Linear( W_out * H_out * N_channel, N_classes)
         self.fc = nn.Linear(6 * 6 * 64, 10)
 
+        
         # The blog used MSE and Softmax as loss function
         # In this code, I will use CrossEntropyLoss to do them at the same time
+        '''
+
+        # For the sake of accuracy, I will use deeper network
+        # The net that the blog used has about 40% accuracy
+        # This one has about 88% on 5 epochs
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(3,3), stride=(2,2), padding=1)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(5,5), stride=(2,2), padding=2)
+        self.fc1 = nn.Linear(7*7*64, 32)
+        self.fc2 = nn.Linear(32, 16)
+        self.fc3 = nn.Linear(16, 10)
 
     def forward(self, x):
         x = f.relu(self.conv1(x))
@@ -67,9 +79,11 @@ class ConvNet(nn.Module):
         # Batch Normalization(x)
 
         # Flattening
-        x = x.view(-1, 6 * 6 * 64)
+        x = x.view(x.size(0), -1)
         # default drop-out probability = 0.5
-        x = f.dropout(f.relu(self.fc(x)))
+        x = f.dropout(f.relu(self.fc1(x)))
+        x = f.relu(self.fc2(x))
+        x = self.fc3(x)
 
         return x
 
@@ -101,18 +115,39 @@ def training_loop(n_epoch, network, optim_fn, loss_fn, data_t):
 
 # We pass an image through the trained-model, and calculate the loss of the image
 # Then, we use the sign of the gradiant of loss to generate adversarial attack
-def generate_image_adversary(model, loss_fn, data, batch_size, eps=2/255.0):
-    for img, label in data:
-        img = img.to(device)
-        label = label.to(device)
 
+# adv=original+ϵ∗sign(∇xJ(x,θ))
+def generate_image_adversary(model, loss_fn, data, eps=0.01):
+    img, label = data
+    img = img.view(1, 1, 28, 28)
+    img = img.to(device)
+    label = torch.Tensor([label]).long().to(device)
 
+    img.requires_grad = True
+
+    model.zero_grad()
+    pred = model(img).to(device)
+
+    loss = loss_fn(pred, label).to(device)
+    # we need to calculate ∇xJ(x,θ)
+    loss.backward()
+    img.requires_grad = False
+
+    '''
+    plt.imshow(img.grad.data.to('cpu').view(28,28))
+    plt.show()
+    '''
+    tmp = img + eps*img.grad.data.sign()
+    '''
+    plt.imshow(tmp.to('cpu').view(28,28))
+    plt.show()
+    '''
+
+    return tmp
 
 
 print('Training the model')
-
-# run
-n_epochs = 5
+n_epochs = 1
 training_loop(
     n_epoch=n_epochs,
     network=model,
@@ -121,7 +156,6 @@ training_loop(
     data_t=data_t)
 
 print('Calculating Accuracy...')
-
 with torch.no_grad():
     n_correct = 0
     n_samples = 0
@@ -137,10 +171,7 @@ with torch.no_grad():
         n_samples += labels.size(0)
         n_correct += (predicted == labels).sum().item()
 
-        if len(images) != batch_size:
-            break
-
-        for i in range(batch_size):
+        for i in range(images.size(0)):
             label = labels[i]
             pred = predicted[i]
 
@@ -155,16 +186,24 @@ with torch.no_grad():
         acc = 100.0 * n_class_correct[i] / n_class_samples[i]
         print(f'Accuracy of {class_name[i]}: {acc} %')
 
-with torch.no_grad():
-    for k in range(5):
-        tmp = data_val.dataset[k][0]
-        target = data_val.dataset[k][1]
-        plt.imshow(tmp.view(28, 28))
-        plt.show()
-        tmp = tmp.view(1, 1, 28, 28)
-        tmp = tmp.to(device)
-        outputs = model(tmp).to(device)
-        # max returns (value ,index)
-        _, predicted = torch.max(outputs, 1)
-        print('target', target)
-        print(class_name[predicted[0]])
+for k in range(5):
+    n = random.randint(1, 9999)
+    target = data_val.dataset[n][:]
+    adv_img = generate_image_adversary(model=model, loss_fn=loss, data=target)
+
+    img, label = target
+    img = img.view(1, 1, 28, 28).to(device)
+    adv_img = adv_img.view(1, 1, 28, 28).to(device)
+
+    output = model(img).to(device)
+    _, img_pred = torch.max(output, 1)
+
+    adv_output = model(adv_img)
+    _, adv_pred = torch.max(adv_output, 1)
+    img = img.to('cpu').view(28, 28)
+
+    print('target', label)
+    print('without attack:', class_name[img_pred[0]])
+    print('with attck:', class_name[adv_pred[0]])
+    plt.imshow(img)
+    plt.show()
